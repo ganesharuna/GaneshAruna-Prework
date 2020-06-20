3c4e12cacc1ba3ed4a009333fb83e4becf95aa77
Ganesh <ganesh528808@gmail.com>
2020-06-20 11:40:53 -0500

    files

--- Module2_Assessment/assets/javascript/game.js
+++ Module2_Assessment/assets/javascript/game.js
@@ -1,7 +1,8 @@
-let teams, guessword, underscores, underscores2, underscores3,lettersGuessed, numberGuessesLeft;
+let teams, guessword, underscores, blanks,lettersGuessed, numberGuessesLeft, guess;
 var wins=0;
 
 function initialize(){
+guess='';
 lettersGuessed=[];
 numberGuessesLeft=8;
 document.querySelector('#guessnumber').innerHTML=numberGuessesLeft;
@@ -15,15 +16,15 @@ guessword="";
 
 guessword=teams[Math.floor(Math.random()*teams.length)];
 console.log(guessword);
-underscores="";
+underscores=[];
 
 for (i=0;i<guessword.length;i++){
-    underscores+="_ ";
+    underscores.push("_ ");
 }
 
-underscores2=underscores.split(",");
-
-document.querySelector('#guessword').innerHTML=underscores;
+console.log(underscores)
+blanks=underscores.join('');
+document.querySelector('#guessword').innerHTML=blanks;
 console.log(numberGuessesLeft);
 }
 
@@ -34,39 +35,34 @@ console.log(numberGuessesLeft);
 
 initialize();
 
-var letters=["q","w","e","r","t","y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
-
+var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 document.onkeypress=function(event){
-    const key=event.key
+    let key=event.key
     if (letters.indexOf(key)!=-1);{
         guess=key;
         console.log(guess);
     }
         for (var i=0; i<guessword.length;i++){
             if (guess===guessword[i]){
-                underscores2[i]=guess;
-                let underscores3=underscores2.join();
-                underscores3 = underscores3.replace(/,/g,"");
-                console.log(underscores2)
-                console.log(underscores3)
-                document.querySelector('#guessword').innerText=underscores3;
+                underscores[i]=guess;
+                blanks=underscores.join('');
+                console.log(underscores)
+                console.log(blanks)
+                document.querySelector('#guessword').innerText=blanks;
+                if (blanks===guessword){
+                    document.querySelector('#guessword').innerText=guessword;
+                    wins++;
+                    document.querySelector('#wins').innerText=wins;
+                    initialize();
+                }
 
             }
         }
             
-            if ((underscores2.length*2)===underscores.length){
-                let underscores3=underscores2.join();
-                underscores3 = underscores3.replace(/,/g,"");
-                if (underscores3.length==guessword.length){
-                document.querySelector('#guessword').innerText=guessword;
-                wins++;
-                document.querySelector('#wins').innerText=wins;
-                setTimeout(initialize(),30000)
-                }
 
 
-            }
+
         if (!guessword.includes(guess)){
             if (!lettersGuessed.includes(guess)){
             lettersGuessed.push(guess);
