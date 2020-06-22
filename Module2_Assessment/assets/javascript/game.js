f285e188feb0469fef8543507a9f1abe4d97a2a0
Ganesh <ganesh528808@gmail.com>
2020-06-22 08:50:26 -0500

    files

--- Module2_Assessment/assets/javascript/game.js
+++ Module2_Assessment/assets/javascript/game.js
@@ -1,4 +1,4 @@
-let teams, guessword, underscores, blanks,lettersGuessed, numberGuessesLeft, guess;
+let teams, guessword, underscores, blanks,lettersGuessed, numberGuessesLeft, guess,correct;
 var wins=0;
 
 function initialize(){
@@ -13,9 +13,10 @@ teams=["hawks","celtics","nets","hornets","bulls","cavaliers","mavericks","nugge
 document.querySelector('#guessword').innerText="";
 
 guessword="";
-
 guessword=teams[Math.floor(Math.random()*teams.length)];
+correct=guessword;
 console.log(guessword);
+
 underscores=[];
 
 for (i=0;i<guessword.length;i++){
@@ -28,20 +29,35 @@ document.querySelector('#guessword').innerHTML=blanks;
 console.log(numberGuessesLeft);
 }
 
+const imgArray=['assets/images/nba.jpg','assets/images/nba2.jpg','assets/images/nba3.jpg','assets/images/nba4.jpg','assets/images/nba5.jpg','assets/images/nba6.jpg','assets/images/nba7.jpg','assets/images/nba8.jpg','assets/images/nba9.jpg','assets/images/nba10.jpg','assets/images/nba11.jpg','assets/images/nba12.jpg','assets/images/nba13.jpg','assets/images/nba14.jpg'];
 
+let index=0;
+let img=document.getElementById('img');
 
+function cycle(){
+    if (index<imgArray.length){
+        img.src=imgArray[index];
+        index++;
+    }else{
+        index=0;
+    }
+    setTimeout(cycle,2500);
+}
 
 
 
 initialize();
+cycle();
 
 var letters=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 document.onkeypress=function(event){
     let key=event.key
-    if (letters.indexOf(key)!=-1);{
+    if (letters.indexOf(key)!=-1){
         guess=key;
         console.log(guess);
+    }else if (letters.indexOf(key)=-1){
+        event.preventDefault;
     }
         for (var i=0; i<guessword.length;i++){
             if (guess===guessword[i]){
@@ -52,6 +68,7 @@ document.onkeypress=function(event){
                 document.querySelector('#guessword').innerText=blanks;
                 if (blanks===guessword){
                     document.querySelector('#guessword').innerText=guessword;
+                    document.querySelector('#previous').innerText=correct;
                     wins++;
                     document.querySelector('#wins').innerText=wins;
                     initialize();
@@ -75,6 +92,7 @@ document.onkeypress=function(event){
          }
         if (numberGuessesLeft===0){
             document.querySelector('#guessword').innerHTML=guessword;
+            document.querySelector('#previous').innerText=correct;
             setTimeout(initialize(),30000)
 
         }
